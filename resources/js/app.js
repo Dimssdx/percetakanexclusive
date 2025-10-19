30c9fa04165d4c12928174477ba390926e89c3f3
Zkarl9 <haikalazzrial345@gmail.com>
2025-10-19 23:36:45 +0700

    beranda

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
+// resources/js/app.js
+
 import './bootstrap';
+
+import Alpine from 'alpinejs'; // Tambahkan ini
+window.Alpine = Alpine;       // Tambahkan ini
+
+Alpine.start();               // Tambahkan ini
